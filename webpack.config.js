0cfb890b4392f0f8d04cedfebb5b37da466157b8
udaypydi <udaypydi333@gmail.com>
2020-05-10 23:40:22 +0530

    webpack config added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -23,5 +25,12 @@ module.exports = {
                 ],
             },
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Uday Bhaskhar',
+            inject: true
+        })
+    ]
 }
